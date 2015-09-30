3048000370fee6b5889bc32af2a18d6b14e876aa
Matt Thornback <mthornba@opentext.com>
2015-09-30 12:51:39 -0400

    Allow alias to accept arguments

--- lib/lita/alias/chat_handler.rb
+++ lib/lita/alias/chat_handler.rb
@@ -57,7 +57,8 @@ module Lita
 
       def trigger_alias(response)
         ac = alias_store.lookup(response.match_data[1])
-        message = Lita::Message.new(robot, "#{robot.mention_name} #{ac.command}", response.message.source)
+        opts = response.args.join("' '")
+        message = Lita::Message.new(robot, "#{robot.mention_name} #{ac.command} #{opts}", response.message.source)
         robot.receive(message)
       end
 
